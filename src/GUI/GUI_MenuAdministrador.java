305addebcdc1e5abd438ed3e0578e7d69752130f
Countrypie <xavigl7777@gmail.com>
2023-04-25 22:28:52 +0200

    Corregir  menu superusuario
    Se me olvido que metiera en Usuarios, actualice nombres de campos que se cambiaron en el script. Tambien añadi la conexion por defecto al "properties", y renombre el constructor solo para que funcionara

--- src/GUI/GUI_MenuAdministrador.java
+++ src/GUI/GUI_MenuAdministrador.java
@@ -51,7 +51,7 @@ public class GUI_MenuAdministrador extends javax.swing.JDialog {
         try{
             
         //Se obtienen todos los cines
-        PreparedStatement s1=c.prepareStatement("SELECT nombre, ciudad, id_cine FROM cine");        
+        PreparedStatement s1=c.prepareStatement("SELECT nombre, ciudad, id FROM cine");        
         ResultSet cines=s1.executeQuery();        
         while(cines.next()){
             jComboBox3.addItem(cines.getString(1)+", "+cines.getString(2));
@@ -455,10 +455,10 @@ public class GUI_MenuAdministrador extends javax.swing.JDialog {
             if(jComboBox2.getSelectedItem() == "Trabajador"){
 
                 PreparedStatement s=this.conexion.prepareStatement("SELECT "
-                        + "nombre, apellido1, apellido2, correocorporativo, "
+                        + "nombre, apellido1, apellido2, correo_electronico, "
                         + "experiencia, telefono " 
                         +"FROM trabajador t, trabajar t2 " 
-                        +"WHERE t.id_trabajador = t2.id_trabajador and t2.id_cine = ?");
+                        +"WHERE t.id = t2.id_trabajador and t2.id_cine = ?");
                 
                 //Se busca el id del cine seleccionado
                 int id = 0,i;
@@ -485,10 +485,10 @@ public class GUI_MenuAdministrador extends javax.swing.JDialog {
             }else{
 
                 PreparedStatement s=this.conexion.prepareStatement("SELECT nombre,"
-                        + " apellido1, apellido2, correocorporativo, experiencia, d.numidiomas, telefono "
+                        + " apellido1, apellido2, correo_electronico, experiencia, d.idiomas, telefono "
                         +"FROM trabajador t, trabajar t2, dependiente d " 
-                        +"WHERE t.id_trabajador = t2.id_trabajador and t2.id_cine = ? " 
-                        +"and t.id_trabajador =d.id_trabajador ");
+                        +"WHERE t.id = t2.id_trabajador and t2.id_cine = ? " 
+                        +"and t.id =d.id ");
                 
                 //Se busca el id del cine seleccionado
                 int id = 0,i;
@@ -555,7 +555,7 @@ public class GUI_MenuAdministrador extends javax.swing.JDialog {
             }
 
             //Se le asigna un id
-            PreparedStatement s=this.conexion.prepareStatement("SELECT max(id_trabajador) "
+            PreparedStatement s=this.conexion.prepareStatement("SELECT max(id) "
                 + "FROM trabajador");
 
             ResultSet maxId=s.executeQuery();
@@ -591,6 +591,12 @@ public class GUI_MenuAdministrador extends javax.swing.JDialog {
                 s.execute();
             }
 
+            //Se le inserta en la tabla de usuarios
+            s=this.conexion.prepareStatement("INSERT INTO usuarios VALUES(?,?)");
+            s.setString(1, correo);
+            s.setString(2, "Dependiente");
+            s.execute();
+            
             //Se le crea una conexion
             String sql="CREATE USER \""+correo+"\" IN ROLE Dependiente PASSWORD '"+contrasena+"'";
             s=this.conexion.prepareStatement(sql);
